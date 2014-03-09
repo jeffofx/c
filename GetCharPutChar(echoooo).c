a89b65cab10a77ff7aaa9805f580923ea0a38be8
marcumjl <marcum@hush.com>
2014-03-09 11:25:51 +1100

    counters; mar08s2
    more K&R exercises

---
+++ GetCharPutChar(echoooo).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*written on phone*/
+main ()
+{
+
+int c;
+  c=getchar ();
+
+  while (c != EOF) {
+   putchar (c);
+   c=getchar ();
+		   }
+}
---
+++ counter_.c
@@ -0,0 +1,20 @@
+//doesn't work 140307f
+
+
+#include <stdio.h>
+
+
+main()
+
+{
+
+	double nc;
+
+	for (nc = 0; getchar () != EOF; ++nc)
+
+		;
+
+	printf ("%.0f\n", nc);
+
+
+}
